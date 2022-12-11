2d006a5a51704b43a6be621ede9f82764354084d
Aaron Burzak <aaron.j.burzak@gmail.com>
2022-12-11 09:47:48 -0500

    added icons to todo list

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import Todo from './Todo'
 import TodoForm from './TodoForm'
 
 function TodoList() {
@@ -17,11 +18,22 @@ function TodoList() {
     }
 
 
+const completeTodo = id => {
+    let updatedTodos = todos.map(todo => {
+        if (todo.id === id){
+            todo.isComplete = !todo.isComplete
+        }
+        return todo
+    })
+    setTodos(updatedTodos);
+}
+
 
   return (
     <div>
         <h1>To Do List</h1>
         <TodoForm onSubmit={addTodo} />
+        <Todo todos={todos} completeTodos={completeTodo} />
     </div> 
   )
 }
